dbda1cdd134405c071dc26d0d587af56d03db795
Chris Row <1418370+memblin@users.noreply.github.com>
2024-08-17 21:11:37 -0600

    feat: integrated /etc/hosts snippet output
      - Add new generate_hosts function
      - Add new hosts jinja template
      - Add new cli hosts command
      - Implement hosts --append
      - Implement hosts --heredoc
      - fix: ignore dhcp machines in hosts command
      - chore: bump package version for relase

--- tkc_lvlab/config.py
+++ tkc_lvlab/config.py
@@ -1,7 +1,39 @@
 import os
+from jinja2 import Environment, PackageLoader, select_autoescape
 from urllib.parse import urlparse
 
+import click
 import yaml
+
+
+def generate_hosts(environment, config_defaults, machines):
+    """Generte hosts file entry content from config
+
+    Parses the manifest to create sets of /etc/hosts
+    entries for each defined machine.
+
+    Output created by rendering a jinja template
+    with the data
+    """
+    env = Environment(loader=PackageLoader("tkc_lvlab"), autoescape=select_autoescape())
+
+    hosts = []
+    for machine in machines:
+        if len(machine.get("interfaces", [])) > 0 and machine["interfaces"][0].get(
+            "ip4", None
+        ):
+            hosts_entry = {
+                "ip4": machine["interfaces"][0]["ip4"].split("/")[0],
+                "fqdn": machine["hostname"] + "." + config_defaults["domain"],
+                "hostname": machine["hostname"],
+            }
+            hosts.append(hosts_entry)
+
+    config = {"env": environment, "defaults": config_defaults, "hosts": hosts}
+    template_file = "hosts.j2"
+    template = env.get_template(template_file)
+
+    return template.render(config=config)
 
 
 def parse_config(fpath=None):
